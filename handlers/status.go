ead10cc1095b76dbd370f60a34565881373ad759
jp <jan-paul.konijn@tno.nl>
2024-06-18 17:55:12 +0200

    renaming of handler file + env file example + utility for env file reading

---
+++ handlers/status.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+type StatusHandler struct{}
+
+func (s *StatusHandler) StatusIndicator(context *gin.Context) {
+}
--- server/main.go
+++ server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"soarca-gui/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -12,6 +15,12 @@ var (
 )
 
 func main() {
+	errenv := godotenv.Load(".env")
+
+	if errenv != nil {
+		fmt.Println("Failed to read env variable, but will continue")
+	}
+
 	app := gin.Default()
 	routes.Setup(app)
 
