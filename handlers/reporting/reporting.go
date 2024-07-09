82392977589da0613b6c721c770f4f691cebd1df
jp <jan-paul.konijn@tno.nl>
2024-07-09 17:00:11 +0200

    added getReport function

--- handlers/reporting/reporting.go
+++ handlers/reporting/reporting.go
@@ -1,6 +1,33 @@
 package reporting
 
+import (
+	"fmt"
+	"net/http"
+
+	"soarca-gui/models"
+	utils "soarca-gui/utils"
+)
+
 const (
-	reportingApiPAth   = "/reporter"
+	reportingApiPath   = "/reporter"
 	reportingApiPathId = "/reporter/:id"
 )
+
+type reportingHandler struct {
+	Host string
+}
+
+func NewReportingHandler(host string) reportingHandler {
+	return reportingHandler{Host: host}
+}
+
+func (r *reportingHandler) getReports() ([]models.PlaybookExecutionReport, error) {
+	var response []models.PlaybookExecutionReport
+	url := fmt.Sprintf("%s%s", r.Host, reportingApiPath)
+
+	reports, err := utils.MakeJsonRequest(url, http.MethodGet, nil, response)
+	if err != nil {
+		return nil, err
+	}
+	return reports, nil
+}
