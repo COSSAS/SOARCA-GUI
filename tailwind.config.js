c4c9a6cbed29bc24055e8a7b884ba6178de5bc71
JP <jpkonijn@protonmail.ch>
2024-09-06 08:40:33 +0200

    adding custom color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
 	content: ["./**/*.templ", "./node_modules/flowbite/**/*.js"],
 
@@ -8,6 +9,16 @@ module.exports = {
 		fontFamily: {
 		  sans: ['Inter', 'sans-serif'],
 		},
+		colors: {
+			'v-blue-deep': colors.blue[600],  // Adding only blue-400 to the custom schema		
+			'v-blue-light-backgroud': color.blue[50],
+			
+			'v-beige-dark': '#A39564',
+			'v-blue-light': '#8089E8',
+			'v-blue-dark': '#3B428F',
+			'v-brown': '#665825',
+			'v-grey-dark': '#282A38',
+				}
 	  },
 	},
 	plugins: [
