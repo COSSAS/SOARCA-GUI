6255734d8732b415ec32b6c4af317b780f0aaac4
JP <jpkonijn@protonmail.ch>
2024-09-06 09:20:56 +0200

    using custom color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,11 @@ module.exports = {
 		  sans: ['Inter', 'sans-serif'],
 		},
 		colors: {
-			'v-blue-deep': colors.blue[600],  // Adding only blue-400 to the custom schema		
-			'v-blue-light-backgroud': color.blue[50],
-			
-			'v-beige-dark': '#A39564',
-			'v-blue-light': '#8089E8',
-			'v-blue-dark': '#3B428F',
-			'v-brown': '#665825',
-			'v-grey-dark': '#282A38',
-				}
+			'c-blue-deep-light': colors.blue[500],  
+			'c-blue-deep': colors.blue[600],  
+			'c-blue-deep-extra': colors.blue[700],		
+			'c-blue-light-backgroud': colors.blue[50],
+		}
 	  },
 	},
 	plugins: [
