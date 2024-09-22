7241859aff052d458b881a4c4acc6c1118136241
JP <jpkonijn@protonmail.ch>
2024-09-22 20:57:46 +0200

    bug fixed and clean up on the auth setup function

--- auth/auth.go
+++ auth/auth.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	OIDC_REDIRECT_URL = "/oidc-login"
+	OIDC_CALL_BACK = "/oidc-callback"
 )
 
 type Authenticator struct {
@@ -23,74 +23,97 @@ type Authenticator struct {
 	OIDCconfig       *oidc.Config
 	OauthConfig      *oauth2.Config
 	verifierProvider *oidc.Provider
+	skipTLSVerify    bool
 }
 
 func SetupOIDCAuthHandler() *Authenticator {
-	providerLink := utils.GetEnv("OIDC_PROVIDER", "")
-	soarcaGUIDomain := utils.GetEnv("SOARCA_GUI_URI", "http://localhost:8081")
-	clientID := utils.GetEnv("OIDC_CLIENT_ID", "")
-	clientSecret := utils.GetEnv("OIDC_CLIENT_SECRET", "")
-	skipTLSVerify := utils.GetEnv("OIDC_SKIP_TLS_VERIFY", "false")
-	cookieJarSecret := utils.GetEnv("COOKIE_SECRET_KEY", "")
-
-	// Environment variable checks
-	if providerLink == "" {
-		log.Fatal("invalid provider link for the env: OIDC_PROVIDER")
+	env := struct {
+		providerLink    string
+		soarcaGUIDomain string
+		clientID        string
+		clientSecret    string
+		skipTLSVerify   string
+		cookieJarSecret string
+	}{
+		providerLink:    utils.GetEnv("OIDC_PROVIDER", ""),
+		soarcaGUIDomain: utils.GetEnv("SOARCA_GUI_URI", "http://localhost:8081"),
+		clientID:        utils.GetEnv("OIDC_CLIENT_ID", ""),
+		clientSecret:    utils.GetEnv("OIDC_CLIENT_SECRET", ""),
+		skipTLSVerify:   utils.GetEnv("OIDC_SKIP_TLS_VERIFY", "false"),
+		cookieJarSecret: utils.GetEnv("COOKIE_SECRET_KEY", ""),
 	}
 
-	if clientID == "" {
-		log.Fatal("invalid oidc client ID for the env: OIDC_CLIENT_ID")
-	}
-	if clientSecret == "" {
-		log.Fatal("invalid oidc client secret for the env: OIDC_CLIENT_SECRET")
-	}
-	if cookieJarSecret == "" || len(cookieJarSecret) < 32 {
-		log.Fatal("invalid cookie secret key for the env: COOKIE_SECRET_KEY. Note: should be at least 33 characters")
-	}
-
-	skipTLS, err := strconv.ParseBool(skipTLSVerify)
+	validateEnvVariables(env)
+	skipTLS, err := strconv.ParseBool(env.skipTLSVerify)
 	if err != nil {
 		log.Printf("Invalid SKIP_TLS_VERIFY value. Defaulting to false. Error: %v", err)
 		skipTLS = false
 	}
 
-	var client *http.Client
-	if skipTLS {
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		client = &http.Client{Transport: tr}
-		log.Println("Warning: TLS verification is disabled. This should not be used in production.")
-	} else {
-		client = http.DefaultClient
-	}
-
+	client := setupHTTPClient(skipTLS)
 	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, client)
-
-	provider, err := oidc.NewProvider(ctx, providerLink)
+	provider, err := oidc.NewProvider(ctx, env.providerLink)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to create OIDC provider: %v", err)
 	}
 
-	oidcConfig := &oidc.Config{
-		ClientID: clientID,
-	}
-
+	oidcConfig := &oidc.Config{ClientID: env.clientID}
 	oauthConfig := &oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		RedirectURL:  fmt.Sprintf("%s%s", soarcaGUIDomain, OIDC_REDIRECT_URL),
+		ClientID:     env.clientID,
+		ClientSecret: env.clientSecret,
+		RedirectURL:  fmt.Sprintf("%s%s", env.soarcaGUIDomain, OIDC_CALL_BACK),
 		Endpoint:     provider.Endpoint(),
 		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
 	}
 
-	cookieJar := cookies.NewCookieJar([]byte(cookieJarSecret))
+	cookieJar := cookies.NewCookieJar([]byte(env.cookieJarSecret))
 
-	return NewAuthenticator(cookieJar, oidcConfig, oauthConfig, provider)
+	return NewAuthenticator(cookieJar, oidcConfig, oauthConfig, provider, skipTLS)
 }
 
-func NewAuthenticator(cj cookies.ICookieJar, OIDCconfig *oidc.Config, OauthConfig *oauth2.Config, verifierProvider *oidc.Provider) *Authenticator {
-	return &Authenticator{Cookiejar: cj, OIDCconfig: OIDCconfig, OauthConfig: OauthConfig, verifierProvider: verifierProvider}
+func validateEnvVariables(env struct {
+	providerLink    string
+	soarcaGUIDomain string
+	clientID        string
+	clientSecret    string
+	skipTLSVerify   string
+	cookieJarSecret string
+},
+) {
+	if env.providerLink == "" {
+		log.Fatal("invalid provider link for the env: OIDC_PROVIDER")
+	}
+	if env.clientID == "" {
+		log.Fatal("invalid oidc client ID for the env: OIDC_CLIENT_ID")
+	}
+	if env.clientSecret == "" {
+		log.Fatal("invalid oidc client secret for the env: OIDC_CLIENT_SECRET")
+	}
+	if env.cookieJarSecret == "" || len(env.cookieJarSecret) < 32 {
+		log.Fatal("invalid cookie secret key for the env: COOKIE_SECRET_KEY. Note: should be at least 33 characters")
+	}
+}
+
+func setupHTTPClient(skipTLS bool) *http.Client {
+	if skipTLS {
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+		log.Println("Warning: TLS verification is disabled. This should not be used in production.")
+		return client
+	}
+	return http.DefaultClient
+}
+
+func NewAuthenticator(cj cookies.ICookieJar, OIDCconfig *oidc.Config, OauthConfig *oauth2.Config, verifierProvider *oidc.Provider, skipTLSVerify bool) *Authenticator {
+	return &Authenticator{
+		Cookiejar:        cj,
+		OIDCconfig:       OIDCconfig,
+		OauthConfig:      OauthConfig,
+		verifierProvider: verifierProvider,
+		skipTLSVerify:    skipTLSVerify,
+	}
 }
 
 func (auth *Authenticator) GetTokenVerifier() *oidc.IDTokenVerifier {
--- auth/gin_oidc.go
+++ auth/gin_oidc.go
@@ -1,12 +1,15 @@
 package auth
 
 import (
+	"context"
+	"crypto/tls"
 	"errors"
 	"net/http"
 	"soarca-gui/auth/api"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
 )
 
 const (
@@ -14,68 +17,71 @@ const (
 	CALLBACK_NONCE = "soarca_gui_nonce"
 )
 
-func (auth *Authenticator) OIDCRedirectToLogin(context *gin.Context) {
-	state, err := randString(32)
+func (auth *Authenticator) OIDCRedirectToLogin(gin *gin.Context) {
+	state, err := randString(16)
 	if err != nil {
-		api.JSONErrorStatus(context, http.StatusInsufficientStorage, errors.New("failed to generate state"))
+		api.JSONErrorStatus(gin, http.StatusInsufficientStorage, errors.New("failed to generate state"))
 		return
 	}
 	nonce, err := randString(16)
 	if err != nil {
-		api.JSONErrorStatus(context, http.StatusInsufficientStorage, errors.New("failed to generate nonce"))
+		api.JSONErrorStatus(gin, http.StatusInsufficientStorage, errors.New("failed to generate nonce"))
 		return
 	}
-	auth.Cookiejar.SetCallBackState(context, CALLBACK_STATE, state)
-	auth.Cookiejar.SetCallBackState(context, CALLBACK_NONCE, nonce)
+	auth.Cookiejar.SetCallBackState(gin, CALLBACK_STATE, state)
+	auth.Cookiejar.SetCallBackState(gin, CALLBACK_NONCE, nonce)
 
-	context.Redirect(http.StatusFound, auth.OauthConfig.AuthCodeURL(state, oidc.Nonce(nonce)))
+	gin.Redirect(http.StatusFound, auth.OauthConfig.AuthCodeURL(state, oidc.Nonce(nonce)))
 }
 
-func (auth *Authenticator) OIDCCallBack(context *gin.Context) {
-	state, isNew := auth.Cookiejar.StateSession(context, CALLBACK_STATE)
+func (auth *Authenticator) OIDCCallBack(gin *gin.Context) {
+	state, isNew := auth.Cookiejar.StateSession(gin, CALLBACK_STATE)
 	if isNew || state == "" {
-		api.JSONErrorStatus(context, http.StatusInternalServerError, errors.New("state missing"))
+		api.JSONErrorStatus(gin, http.StatusInternalServerError, errors.New("state missing"))
+		return
+	}
+	if state != gin.Query("state") {
+		api.JSONErrorStatus(gin, http.StatusUnauthorized, errors.New("state mismatch"))
 		return
 	}
 
-	cookie, err := context.Request.Cookie(CALLBACK_STATE)
+	localContext := gin.Request.Context()
+	if auth.skipTLSVerify {
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+		localContext = context.WithValue(localContext, oauth2.HTTPClient, client)
+	}
+
+	oauth2Token, err := auth.OauthConfig.Exchange(localContext, gin.Query("code"))
 	if err != nil {
-		api.JSONErrorStatus(context, http.StatusBadRequest, errors.New("state missing from client"))
-		return
-	}
-
-	if cookie.Value != state {
-		api.JSONErrorStatus(context, http.StatusUnauthorized, errors.New("state mismatch"))
-		return
-	}
-
-	oauth2Token, err := auth.OauthConfig.Exchange(context, context.Query("code"))
-	if err != nil {
-		api.JSONErrorStatus(context, http.StatusBadRequest, errors.New("could not get code from URL"))
+		api.JSONErrorStatus(gin, http.StatusBadRequest, errors.New("could not get code from URL"))
 		return
 	}
 	rawIDtoken, ok := oauth2Token.Extra("id_token").(string)
-
 	if !ok {
-		api.JSONErrorStatus(context, http.StatusBadRequest, errors.New("could not obtain code from URL"))
+		api.JSONErrorStatus(gin, http.StatusBadRequest, errors.New("could not parse id_token"))
 		return
 	}
-
 	verifier := auth.GetTokenVerifier()
-	verifiedIDToken, err := verifier.Verify(context, rawIDtoken)
+	verifiedIDToken, err := verifier.Verify(localContext, rawIDtoken)
 	if err != nil {
-		api.JSONErrorStatus(context, http.StatusInternalServerError, errors.New("failed to verify ID token"))
+		api.JSONErrorStatus(gin, http.StatusInternalServerError, errors.New("failed to verify ID token"))
 		return
 	}
-
-	nonce, err := context.Request.Cookie(CALLBACK_NONCE)
+	nonce, isNewNonce := auth.Cookiejar.StateSession(gin, CALLBACK_NONCE)
+	if isNewNonce || state == "" {
+		api.JSONErrorStatus(gin, http.StatusBadRequest, errors.New("state or invalid nonce"))
+		return
+	}
 	if err != nil {
-		api.JSONErrorStatus(context, http.StatusInternalServerError, errors.New("missing id token"))
+		api.JSONErrorStatus(gin, http.StatusInternalServerError, errors.New("missing id token"))
 		return
 	}
-	if verifiedIDToken.Nonce != nonce.Value {
-		api.JSONErrorStatus(context, http.StatusBadRequest, errors.New("nonce for verified id token did not match"))
+	if verifiedIDToken.Nonce != nonce {
+		api.JSONErrorStatus(gin, http.StatusBadRequest, errors.New("nonce for verified id token did not match"))
 		return
 	}
-	context.Redirect(http.StatusOK, "/dashboard")
+	gin.Redirect(http.StatusFound, "/dashboard")
 }
