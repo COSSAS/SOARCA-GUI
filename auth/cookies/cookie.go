840f4240ca74995411f77ea8d4c6b1f4516205c0
JP <jpkonijn@protonmail.ch>
2024-10-10 22:16:38 +0200

    rewrite work on the cookie, after feedback unfinished, requires tests

--- auth/cookies/cookie.go
+++ auth/cookies/cookie.go
@@ -1,6 +1,8 @@
 package cookies
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +15,18 @@ const (
 	USER_TOKEN     = "soarca_token"
 )
 
+type CookieType uint
+
+const (
+	State CookieType = iota
+	Nonce
+	Token
+)
+
+type Cookie struct {
+	CookieType CookieType
+	Value      string
+}
 type ICookieJar interface {
 	SetCallBackNonce(context *gin.Context, stateValue string) error
 	SetCallBackState(context *gin.Context, stateValue string) error
@@ -23,9 +37,132 @@ type ICookieJar interface {
 	DeleteStateSession(context *gin.Context) error
 	DeleteNonceSession(context *gin.Context) error
 }
-
+type ICookieJarNew interface {
+	Store(context *gin.Context, cookie Cookie) error
+	NewCookie(cookieType CookieType) Cookie
+	Delete(context *gin.Context, cookie Cookie)
+}
 type CookieJar struct {
 	store sessions.Store
+}
+
+func NewCookie(cookieType CookieType, value string) (Cookie, error) {
+	if value == "" {
+		return Cookie{}, fmt.Errorf("cookie value cannot be empty")
+	}
+
+	switch cookieType {
+	case State, Nonce, Token:
+	default:
+		return Cookie{}, fmt.Errorf("invalid cookie type: %d", cookieType)
+	}
+
+	return Cookie{
+		CookieType: cookieType,
+		Value:      value,
+	}, nil
+}
+
+func (cj *CookieJar) Delete(gc *gin.Context, cookieType CookieType) error {
+	var sessionName string
+
+	switch cookieType {
+	case Nonce:
+		sessionName = CALLBACK_NONCE
+	case State:
+		sessionName = CALLBACK_STATE
+	case Token:
+		sessionName = USER_TOKEN
+	default:
+		return errors.New("no correct cookie type has been supplied, should be of type: Nonce | State | Token")
+	}
+
+	session, err := cj.store.Get(gc.Request, sessionName)
+	if err != nil {
+		return err
+	}
+
+	session.Options.MaxAge = -1
+
+	switch cookieType {
+	case Nonce:
+		delete(session.Values, "nonce")
+	case State:
+		delete(session.Values, "state")
+	case Token:
+		delete(session.Values, "token")
+	}
+
+	return session.Save(gc.Request, gc.Writer)
+}
+
+func (cj *CookieJar) Store(gc *gin.Context, cookie Cookie) error {
+	var session *sessions.Session
+	var err error
+
+	switch cookie.CookieType {
+	case Nonce:
+		session, err = cj.store.Get(gc.Request, CALLBACK_NONCE)
+		if err != nil {
+			return err
+		}
+		session.Values["nonce"] = cookie.Value
+		session.Options.MaxAge = 60 * 5
+	case State:
+		session, err = cj.store.Get(gc.Request, CALLBACK_STATE)
+		if err != nil {
+			return err
+		}
+		session.Values["state"] = cookie.Value
+		session.Options.MaxAge = 60 * 5
+	case Token:
+		session, err = cj.store.Get(gc.Request, USER_TOKEN)
+		if err != nil {
+			return err
+		}
+		session.Values["token"] = cookie.Value
+		session.Options.MaxAge = 60 * 60 * 8
+	default:
+		return errors.New("no correct cookie type has been supplied, should be of type: Nonce | State | Token")
+	}
+
+	session.Options.Path = "/"
+	session.Options.Secure = gc.Request.TLS != nil
+	session.Options.SameSite = http.SameSiteLaxMode
+
+	return session.Save(gc.Request, gc.Writer)
+}
+
+func (cj *CookieJar) Get(gc *gin.Context, cookieType CookieType) (value string, isNew bool, err error) {
+	var sessionName string
+
+	switch cookieType {
+	case Nonce:
+		sessionName = CALLBACK_NONCE
+	case State:
+		sessionName = CALLBACK_STATE
+	case Token:
+		sessionName = USER_TOKEN
+	default:
+		return "", false, errors.New("no correct cookie type has been supplied, should be of type: Nonce | State | Token")
+	}
+
+	session, err := cj.store.Get(gc.Request, sessionName)
+	if err != nil {
+		return "", true, nil
+	}
+
+	val, ok := session.Values[sessionName]
+	if !ok {
+		return "", true, nil
+	}
+
+	value, ok = val.(string)
+	if !ok {
+		return "", false, errors.New("stored value is not a string")
+	}
+
+	return value, false, nil
 }
 
 func NewCookieJar(secret []byte, encryptionKey []byte) *CookieJar {
